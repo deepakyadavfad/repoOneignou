b4a47361d39bfaeec5ff5aa888762fb6ede8df10
deepakyadavfad <deepakyadavfad@gmail.com>
2023-04-06 01:44:40 +0530

    stage 1 completed

--- SVCS/src/main/kotlin/Main.kt
+++ SVCS/src/main/kotlin/Main.kt
@@ -1,7 +1,22 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+const val LENGTH_COMMAND_IN_MENU = 11
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val help = mapOf(
+        "config" to "Get and set a username.",
+        "add" to "Add a file to the index.",
+        "log" to "Show commit logs.",
+        "commit" to "Save changes.",
+        "checkout" to "Restore a file."
+    )
+
+    when {
+        args.isEmpty() || args[0] == "--help" -> {
+            println("These are SVCS commands:")
+            for (i in help.keys) {
+                println("${i}${" ".repeat(LENGTH_COMMAND_IN_MENU - i.length)}${help[i]}")
+            }
+        }
+        help.containsKey(args[0]) -> println(help[args[0]])
+        else -> println("'${args[0]}' is not a SVCS command.")
+    }
 }
